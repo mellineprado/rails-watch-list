51f23707a4a963d8935bcb753bd1795858d60d46
Melline Prado <mellinevprado@hotmail.com>
2022-11-20 15:44:42 -0300

    Update list and bookmark backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
+  resources :lists do
+    resources :bookmarks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  end
 end
