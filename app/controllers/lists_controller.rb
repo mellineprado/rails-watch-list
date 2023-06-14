eee438ab597a3345f96dbebc6bed6aaf3328ad22
melline prado <mellinevprado@hotmail.com>
2023-06-14 14:10:59 -0300

    added css

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,13 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
   end
 
   def new
@@ -26,6 +29,10 @@ class ListsController < ApplicationController
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name, :photo)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root to: "lists#index"
-  resources :lists do
-    resources :bookmarks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
+    resources :reviews, only: :create
   end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
